f0d4f305d781baf40a9324424f8ac9265b42750d
Douuvid <silasiharis@gmail.com>
2023-07-11 14:36:43 +0200

    Modification avec la repartition de chaque action achete

--- brut_4.py
+++ brut_4.py
@@ -7,7 +7,6 @@
 # Ac 2
 # Ac 3
 from itertools import combinations
-from collections import namedtuple
 
 actions = [
     {"name": "Action-1", "cost": 20, "benefice": 5},
@@ -15,7 +14,7 @@ actions = [
     {"name": "Action-3", "cost": 50, "benefice": 15},
 ]
 
-budget = 50
+budget = 100
 
 
 def brute_force(actions, budget):
@@ -29,12 +28,11 @@ def brute_force(actions, budget):
             total_benefit = sum(action['benefice'] for action in combination)
             if total_cost <= budget:
                 all_combinations.append(combination)
-                if total_cost == budget and total_benefit > best_benefit:
+                if total_benefit > best_benefit:
                     best_combination = combination
                     best_benefit = total_benefit
 
     return all_combinations, best_combination, best_benefit
-
 
 
 all_combinations, best_combination, best_benefit = brute_force(actions, budget)
@@ -43,15 +41,26 @@ print(f"Nombre de combinaisons possibles : {len(all_combinations)}")
 print("Toutes les combinaisons possibles :")
 for i, combination in enumerate(all_combinations, 1):
     print(f"Combinaison {i} : {combination}")
+    print("Répartition des actions :")
+    total_cost = sum(action['cost'] for action in combination)
+    for action in combination:
+        action_budget_percentage = (action['cost'] / total_cost) * 100
+        print(f"{action['name']}: {action_budget_percentage:.2f}% du budget de : {budget}, euros")
+    print()
 
 if best_combination is not None:
     best_combination_index = all_combinations.index(best_combination) + 1
-    print(f"\nMeilleure combinaison avec un budget de {budget} :")
+    print(f"Meilleure combinaison avec un budget de {budget} :")
     print(f"Combinaison {best_combination_index} :", best_combination)
     print("Composition de la combinaison :", end=" ")
     for action in best_combination:
         print(action['name'], end=" ")
     print()
     print(f"Bénéfice total : {best_benefit}")
+    print("Répartition des actions :")
+    total_cost = sum(action['cost'] for action in best_combination)
+    for action in best_combination:
+        action_budget_percentage = (action['cost'] / total_cost) * 100
+        print(f"{action['name']}: {action_budget_percentage:.2f}% du budget de : {budget} euros")
 else:
     print("\nAucune combinaison valide avec un budget de", budget, ".")
